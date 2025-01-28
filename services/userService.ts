74524db5be692a49377608bde295a62ae4b7e4f9
khushig8 <khushigarg9785@gmail.com>
2025-01-28 22:19:53 +0530

    File Upload

--- services/userService.ts
+++ services/userService.ts
@@ -1,20 +1,32 @@
 import { firestore } from "@/config/firebase";
 import { ResponseType, UserDataType } from "@/types";
 import { doc, updateDoc } from "firebase/firestore";
+import { uploadFileToCloudinary } from "./imageService";
 
-
-export const updateUser= async(
-    uid:string,
+export const updateUser = async (
+    uid: string,
     updateData: UserDataType
-):Promise<ResponseType>=>{
+): Promise<ResponseType> => {
     try {
-        const userRef =doc(firestore,"users",uid);
-        await updateDoc(userRef,updateData);
+        if (updateData.image && updateData?.image?.uri) {
+            const imageUploadRes = await uploadFileToCloudinary(
+                updateData.image,
+                "users"
+            );
+            if (!imageUploadRes.success) {
+                return {
+                    success: false,
+                    msg: imageUploadRes.msg || "Failed to upload image",
+                };
+            }
+            updateData.image = imageUploadRes.data;
+        }
+        const userRef = doc(firestore, "users", uid);
+        await updateDoc(userRef, updateData);
 
-        return{ success:true, msg: "update successfully"}
-
-    }catch(error:any){
-        console.log('error while updating user: ', error);
+        return { success: true, msg: "update successfully" };
+    } catch (error: any) {
+        console.log("error while updating user: ", error);
         return { success: false, msg: error?.message };
     }
 };
